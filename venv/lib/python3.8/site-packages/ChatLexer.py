6fb7e5c2625919fe327c447a515b63bd9e1d2b2c
Matias Ucker <matias.ucker@gmail.com>
2020-12-01 12:45:15 -0300

    Implementação das Classes

---
+++ venv/lib/python3.8/site-packages/ChatLexer.py
@@ -0,0 +1,116 @@
+# Generated from Chat.g4 by ANTLR 4.8
+from antlr4 import *
+from io import StringIO
+from typing.io import TextIO
+import sys
+
+
+
+def serializedATN():
+    with StringIO() as buf:
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\20")
+        buf.write("z\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7")
+        buf.write("\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t\13\4\f\t\f\4\r\t\r\4\16")
+        buf.write("\t\16\4\17\t\17\4\20\t\20\4\21\t\21\4\22\t\22\4\23\t\23")
+        buf.write("\4\24\t\24\4\25\t\25\4\26\t\26\4\27\t\27\4\30\t\30\3\2")
+        buf.write("\3\2\3\3\3\3\3\4\3\4\3\5\3\5\3\6\3\6\3\7\3\7\3\b\3\b\3")
+        buf.write("\t\3\t\3\n\3\n\3\13\3\13\3\f\3\f\3\r\3\r\3\16\3\16\3\17")
+        buf.write("\3\17\3\20\3\20\3\21\3\21\3\22\3\22\3\23\3\23\3\23\3\23")
+        buf.write("\3\23\3\24\3\24\3\24\3\24\3\24\3\24\3\24\3\25\3\25\6\25")
+        buf.write("b\n\25\r\25\16\25c\3\26\3\26\3\26\6\26i\n\26\r\26\16\26")
+        buf.write("j\3\27\6\27n\n\27\r\27\16\27o\3\30\5\30s\n\30\3\30\3\30")
+        buf.write("\6\30w\n\30\r\30\16\30x\2\2\31\3\3\5\4\7\5\t\6\13\7\r")
+        buf.write("\b\17\t\21\n\23\2\25\2\27\2\31\2\33\2\35\2\37\2!\2#\2")
+        buf.write("%\13\'\f)\r+\16-\17/\20\3\2\r\4\2CCcc\4\2UUuu\4\2[[{{")
+        buf.write("\4\2JJjj\4\2QQqq\4\2WWww\4\2VVvv\3\2c|\3\2C\\\4\2++__")
+        buf.write("\4\2\13\13\"\"\2x\2\3\3\2\2\2\2\5\3\2\2\2\2\7\3\2\2\2")
+        buf.write("\2\t\3\2\2\2\2\13\3\2\2\2\2\r\3\2\2\2\2\17\3\2\2\2\2\21")
+        buf.write("\3\2\2\2\2%\3\2\2\2\2\'\3\2\2\2\2)\3\2\2\2\2+\3\2\2\2")
+        buf.write("\2-\3\2\2\2\2/\3\2\2\2\3\61\3\2\2\2\5\63\3\2\2\2\7\65")
+        buf.write("\3\2\2\2\t\67\3\2\2\2\139\3\2\2\2\r;\3\2\2\2\17=\3\2\2")
+        buf.write("\2\21?\3\2\2\2\23A\3\2\2\2\25C\3\2\2\2\27E\3\2\2\2\31")
+        buf.write("G\3\2\2\2\33I\3\2\2\2\35K\3\2\2\2\37M\3\2\2\2!O\3\2\2")
+        buf.write("\2#Q\3\2\2\2%S\3\2\2\2\'X\3\2\2\2)_\3\2\2\2+h\3\2\2\2")
+        buf.write("-m\3\2\2\2/v\3\2\2\2\61\62\7<\2\2\62\4\3\2\2\2\63\64\7")
+        buf.write("/\2\2\64\6\3\2\2\2\65\66\7+\2\2\66\b\3\2\2\2\678\7*\2")
+        buf.write("\28\n\3\2\2\29:\7]\2\2:\f\3\2\2\2;<\7_\2\2<\16\3\2\2\2")
+        buf.write("=>\7\61\2\2>\20\3\2\2\2?@\7B\2\2@\22\3\2\2\2AB\t\2\2\2")
+        buf.write("B\24\3\2\2\2CD\t\3\2\2D\26\3\2\2\2EF\t\4\2\2F\30\3\2\2")
+        buf.write("\2GH\t\5\2\2H\32\3\2\2\2IJ\t\6\2\2J\34\3\2\2\2KL\t\7\2")
+        buf.write("\2L\36\3\2\2\2MN\t\b\2\2N \3\2\2\2OP\t\t\2\2P\"\3\2\2")
+        buf.write("\2QR\t\n\2\2R$\3\2\2\2ST\5\25\13\2TU\5\23\n\2UV\5\27\f")
+        buf.write("\2VW\5\25\13\2W&\3\2\2\2XY\5\25\13\2YZ\5\31\r\2Z[\5\33")
+        buf.write("\16\2[\\\5\35\17\2\\]\5\37\20\2]^\5\25\13\2^(\3\2\2\2")
+        buf.write("_a\6\25\2\2`b\n\13\2\2a`\3\2\2\2bc\3\2\2\2ca\3\2\2\2c")
+        buf.write("d\3\2\2\2d*\3\2\2\2ei\5!\21\2fi\5#\22\2gi\7a\2\2he\3\2")
+        buf.write("\2\2hf\3\2\2\2hg\3\2\2\2ij\3\2\2\2jh\3\2\2\2jk\3\2\2\2")
+        buf.write("k,\3\2\2\2ln\t\f\2\2ml\3\2\2\2no\3\2\2\2om\3\2\2\2op\3")
+        buf.write("\2\2\2p.\3\2\2\2qs\7\17\2\2rq\3\2\2\2rs\3\2\2\2st\3\2")
+        buf.write("\2\2tw\7\f\2\2uw\7\17\2\2vr\3\2\2\2vu\3\2\2\2wx\3\2\2")
+        buf.write("\2xv\3\2\2\2xy\3\2\2\2y\60\3\2\2\2\n\2chjorvx\2")
+        return buf.getvalue()
+
+
+class ChatLexer(Lexer):
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+    T__0 = 1
+    T__1 = 2
+    T__2 = 3
+    T__3 = 4
+    T__4 = 5
+    T__5 = 6
+    T__6 = 7
+    T__7 = 8
+    SAYS = 9
+    SHOUTS = 10
+    TEXT = 11
+    WORD = 12
+    WHITESPACE = 13
+    NEWLINE = 14
+
+    channelNames = [ u"DEFAULT_TOKEN_CHANNEL", u"HIDDEN" ]
+
+    modeNames = [ "DEFAULT_MODE" ]
+
+    literalNames = [ "<INVALID>",
+            "':'", "'-'", "')'", "'('", "'['", "']'", "'/'", "'@'" ]
+
+    symbolicNames = [ "<INVALID>",
+            "SAYS", "SHOUTS", "TEXT", "WORD", "WHITESPACE", "NEWLINE" ]
+
+    ruleNames = [ "T__0", "T__1", "T__2", "T__3", "T__4", "T__5", "T__6", 
+                  "T__7", "A", "S", "Y", "H", "O", "U", "T", "LOWERCASE", 
+                  "UPPERCASE", "SAYS", "SHOUTS", "TEXT", "WORD", "WHITESPACE", 
+                  "NEWLINE" ]
+
+    grammarFileName = "Chat.g4"
+
+    def __init__(self, input=None, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.checkVersion("4.8")
+        self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
+        self._actions = None
+        self._predicates = None
+
+
+    def sempred(self, localctx:RuleContext, ruleIndex:int, predIndex:int):
+        if self._predicates is None:
+            preds = dict()
+            preds[19] = self.TEXT_sempred
+            self._predicates = preds
+        pred = self._predicates.get(ruleIndex, None)
+        if pred is not None:
+            return pred(localctx, predIndex)
+        else:
+            raise Exception("No registered predicate for:" + str(ruleIndex))
+
+    def TEXT_sempred(self, localctx:RuleContext, predIndex:int):
+            if predIndex == 0:
+                return self._input.LA(-1) == ord('[') or self._input.LA(-1) == ord('(')
+         
+
+
